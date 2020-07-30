bcb1261be0ed48b22d33427e86c7d7323f9fae90
Stefan Gollmer <Stefan.Gollmer57@gmail.com>
2020-07-30 09:41:26 +0200

    Encrypting passwords via the start menu did not work under Windows.

--- src/de/sgollmer/solvismax/Version.java
+++ src/de/sgollmer/solvismax/Version.java
@@ -14,7 +14,7 @@ import java.util.jar.Manifest;
 
 public class Version {
 
-	private String serverVersion = "01.01.03 rc12";
+	private String serverVersion = "01.01.03 rc2";
 	private String appendix = "modbus alpha, MQTT beta";
 
 	public static Version getInstance() {
