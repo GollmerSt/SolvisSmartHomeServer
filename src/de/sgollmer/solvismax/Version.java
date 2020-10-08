805f85069598260a5957b5691f4a907919a33026
Stefan Gollmer <Stefan.Gollmer57@gmail.com>
2020-10-08 21:28:35 +0200

    Preparing V01.02.05

--- src/de/sgollmer/solvismax/Version.java
+++ src/de/sgollmer/solvismax/Version.java
@@ -14,7 +14,7 @@ import java.util.jar.Manifest;
 
 public class Version {
 
-	private String serverVersion = "01.02.05 rc1";
+	private String serverVersion = "01.02.05";
 	private String appendix = "3 heating circuits beta";
 
 	public static Version getInstance() {
